e5a4a4241136c92164c239d97a644048df57b85f
rob <rob.van.bommel@me.com>
2022-06-18 15:55:34 +0200

    commit 4 after merge branch! to master

--- main.py
+++ main.py
@@ -18,6 +18,4 @@ if __name__ == '__main__':
 print('after push to github')
 print('added branch called branch_lambda_1')
 print('added branch_1')
-
-
-
+print('added line 21, because could not commit')
